bb4c2563f4b5b28ef89fdb32215f1eeb71dd66c8
RayToth <t.rajmund.98@gmail.com>
2020-05-02 13:09:49 +0200

    Sort FileWordAnalyzer tests into a new class

--- src/Codecool.FilePartReader.UnitTests/FilePartReaderTests.cs
+++ src/Codecool.FilePartReader.UnitTests/FilePartReaderTests.cs
@@ -6,11 +6,6 @@ using NUnit.Framework;
 
 namespace Codecool.FilePartReader.UnitTests
 {
-    /// <summary>
-    /// When testing a class you should test only that specific class(unit), not the others
-    /// the class may depending on
-    /// Code review can be found in the started pull request
-    /// </summary>
     [TestFixture]
     public class FilePartReaderTest
     {
@@ -20,87 +15,29 @@ namespace Codecool.FilePartReader.UnitTests
         public void Setup()
         {
             _filePartReader = new FilePartReader();
-            _filePartReader.Setup("test.txt", 1, 5);
-            //_filePartReader.Setup();
         }
 
         [Test]
         public void Constructor_Default_FilePathHasDefaultValue()
         {
-            // Arrange
             var defaultPath = string.Empty;
-            // Act
-            _filePartReader.Setup("", 1, 5);
-            // Assert
             Assert.AreEqual(defaultPath, _filePartReader.FilePath);
         }
 
         [Test]
-        public void GetStringsWhichPalindromes_ContainsPalindromes_ReturnPalindromesAsList()
+        public void Setup_FromLineBiggerThanToLine_ThrowsArgumentException()
         {
-            // Arrange
-            var defaultPath = string.Empty;
-            var analyzer = new FileWordAnalyzer(_filePartReader);
-            var expected = new List<string>()
-            {
-                "kajak",
-                "lol",
-                "kerek"
-            };
-
-            // Act
-            var palindromes = analyzer.GetStringsWhichPalindromes();
-
-            // Assert
-            CollectionAssert.AreEqual(expected, palindromes);
+            var fromLine = 5;
+            var toLine = 3;
+            Assert.Throws<ArgumentException>(() => _filePartReader.Setup("test.txt", fromLine, toLine));
         }
 
         [Test]
-        public void Setup_FromLineBiggerThanToLine_HasNoReturn()
+        public void Setup_FromLineIsSmallerThanOne_ThrowsArgumentException()
         {
-            Assert.Throws<ArgumentException>(() => _filePartReader.Setup("test.txt", 5, 3));
+            var fromLine = -1;
+            var toLine = 3;
+            Assert.Throws<ArgumentException>(() => _filePartReader.Setup("test.txt", fromLine, toLine));
         }
-
-        [Test]
-        public void Setup_FromLineIsSmallerThanOne_HasNoReturn()
-        {
-            Assert.Throws<ArgumentException>(() => _filePartReader.Setup("test.txt", -1, 3));
-        }
-
-        [Test]
-        public void GetWordsOrderedAplhabetically_UseMethod_ReturnsListWithOrderedWords()
-        {
-            var analyzer = new FileWordAnalyzer(_filePartReader);
-
-            var orderedWordsList = analyzer.GetWordsOrderedAlphabetically();
-            var expectedWordsList = new List<string>()
-            {
-                "asdasdasd",
-                "Codecool",
-                "kajak",
-                "kerek",
-                "lol",
-                "test"
-            };
-
-            Assert.AreEqual(expectedWordsList, orderedWordsList);
-        }
-
-        [Test]
-        public void GetWordsContainingSubstring_GivenSubstring_ReturnsListWithWordsContainingSubstring()
-        {
-            var analyzer = new FileWordAnalyzer(_filePartReader);
-
-            var subString = "ol";
-            var wordsWithSubstring = analyzer.GetWordsContainingSubstring(subString);
-            var expectedOutPut = new List<string>()
-            {
-                "Codecool",
-                "lol"
-            };
-
-            Assert.AreEqual(expectedOutPut, wordsWithSubstring);
-        }
-
     }
 }
---
+++ src/Codecool.FilePartReader.UnitTests/FileWorldAnalyzerTests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace Codecool.FilePartReader.UnitTests
+{
+    [TestFixture]
+    public class FileWorldAnalyzerTests
+    {
+        FilePartReader _filePartReader;
+        FileWordAnalyzer _fileWorldAnalyzer;
+        [SetUp]
+        public void SetUp()
+        {
+            _filePartReader = new FilePartReader();
+            _filePartReader.Setup("test.txt", 1, 5);
+            _fileWorldAnalyzer = new FileWordAnalyzer(_filePartReader);
+        }
+
+        [Test]
+        public void GetStringsWhichPalindromes_ContainsPalindromes_ReturnPalindromesAsList()
+        {
+            var defaultPath = string.Empty;
+            var expected = new List<string>()
+            {
+                "kajak",
+                "lol",
+                "kerek"
+            };
+
+            var palindromes = _fileWorldAnalyzer.GetStringsWhichPalindromes();
+
+            CollectionAssert.AreEqual(expected, palindromes);
+        }
+
+        [Test]
+        public void GetWordsOrderedAplhabetically_UseMethod_ReturnsListWithOrderedWords()
+        {
+            var orderedWordsList = _fileWorldAnalyzer.GetWordsOrderedAlphabetically();
+            var expectedWordsList = new List<string>()
+            {
+                "asdasdasd",
+                "Codecool",
+                "kajak",
+                "kerek",
+                "lol",
+                "test"
+            };
+
+            Assert.AreEqual(expectedWordsList, orderedWordsList);
+        }
+
+        [Test]
+        public void GetWordsContainingSubstring_GivenSubstring_ReturnsListWithWordsContainingSubstring()
+        {
+            var analyzer = new FileWordAnalyzer(_filePartReader);
+
+            var subString = "ol";
+            var wordsWithSubstring = analyzer.GetWordsContainingSubstring(subString);
+            var expectedOutPut = new List<string>()
+            {
+                "Codecool",
+                "lol"
+            };
+
+            Assert.AreEqual(expectedOutPut, wordsWithSubstring);
+        }
+    }
+}
